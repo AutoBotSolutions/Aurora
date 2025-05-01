6d9f9d5abffff8d773fdc6ad9ece9b39fc73b574
Robert Trenaman <support@autobotsolutions.com>
2025-04-30 22:49:10 -0400

    Update ai_advanced_monitoring.py

--- ai_advanced_monitoring.py
+++ ai_advanced_monitoring.py
@@ -1,5 +1,6 @@
 """
 Home: https://autobotsolutions.com
+Wiki: https://autobotsolutions.com/god/stats/doku.php?id=start
 Project Homepage: https://github.com/AutoBotSolutions/Aurora.git
 License: MIT https://autobotsolutions.com/god/docs/LICENSE
 Maintainer: G.O.D Framework Team
